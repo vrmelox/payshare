3e206f6d9e6ceec60081268f9f1b96703410085f
vrmelo <akandeabiodoun@gmail.com>
2026-02-01 02:33:53 +0100

    feat: Add `RegisterUser` controller function for user registration with input binding and validation.

---
+++ backend/controllers/user.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/vrmelo/payshare/models"
+)
+
+
+func RegisterUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user models.User
+
+		if err := c.ShouldBind(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H {
+				"error": "Invalid input data",
+			})
+			return
+		}
+		validate := validator.New()
+
+		if err := validate.Struct(user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Validation failed",
+				"details": err.Error(),
+			})
+			return
+		}
+		
+	}
+}
